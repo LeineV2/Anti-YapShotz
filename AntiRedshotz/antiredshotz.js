d5da4516ea7ebad6a4c2ff9b0e3c8731a4f79745
LeineV2 <166173225+LeineV2@users.noreply.github.com>
2024-05-13 16:00:01 +0200

    Add files via upload

---
+++ AntiRedshotz/antiredshotz.js
@@ -0,0 +1,25 @@
+const thread = new Thread(() => {
+    ChatLib.say(`MONKEY SPOTTED!`)
+    thread.sleep(600)
+    ChatLib.command(`p leave`)
+})
+
+const thread2 = new Thread(() => {
+    ChatLib.say(`MONKEY SPOTTED!`)
+    thread.sleep(600)
+    ChatLib.command(`p kick redshotz`)
+})
+
+register("chat", () => {
+    if(config.AntiMonkey){
+        thread.start()
+    }
+}).setCriteria("RedShotz's party!").setContains()
+
+register("chat", () => {
+        thread2.start()
+}).setCriteria("RedShotz joined the party.").setContains()
+
+register("chat", (event) => {
+        cancel(event)
+}).setCriteria("RedShotz").setContains()
